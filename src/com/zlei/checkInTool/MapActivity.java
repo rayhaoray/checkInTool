1bd39fb3eae03c297ef699da72fd67d1baa59572
Zhenhao Lei <zhenhaolei@sessionm.com>
2014-10-08 18:38:32 -0400

    WIP

--- src/com/zlei/checkInTool/MapActivity.java
+++ src/com/zlei/checkInTool/MapActivity.java
@@ -15,10 +15,11 @@ import com.google.android.gms.maps.model.MarkerOptions;
 
 import java.util.ArrayList;
 
-public class MapActivity extends Activity implements GoogleMap.OnInfoWindowClickListener{
+public class MapActivity extends Activity implements GoogleMap.OnInfoWindowClickListener {
 
     ArrayList<String> venueNames;
     ArrayList<String[]> venueCoordinates;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,7 +32,7 @@ public class MapActivity extends Activity implements GoogleMap.OnInfoWindowClick
 
         map.setOnInfoWindowClickListener(this);
 
-        if(MVenuesActivity.venueNames != null){
+        if (MVenuesActivity.venueNames != null) {
             venueNames = MVenuesActivity.venueNames;
             venueCoordinates = MVenuesActivity.venueCoordinates;
         }
@@ -43,17 +44,21 @@ public class MapActivity extends Activity implements GoogleMap.OnInfoWindowClick
         map.setMyLocationEnabled(true);
         map.moveCamera(CameraUpdateFactory.newLatLngZoom(core, 16));
 
-        for(String name : venueNames){
+        for (String name : venueNames) {
             int i = venueNames.indexOf(name);
             coreCoor = venueCoordinates.get(i);
             coreLat = Double.valueOf(coreCoor[0]);
             coreLng = Double.valueOf(coreCoor[1]);
             core = new LatLng(coreLat, coreLng);
-            if(coreCoor[2].equals("checkable")) {
+            if (coreCoor[2].equals("checkable")) {
                 map.addMarker(new MarkerOptions()
                         .title(name)
-                        .snippet("The greatest company in the world.")
+                        .snippet("Click to check in here!")
                         .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
+                        .position(core));
+            } else {
+                map.addMarker(new MarkerOptions()
+                        .title(name)
                         .position(core));
             }
         }
@@ -62,9 +67,11 @@ public class MapActivity extends Activity implements GoogleMap.OnInfoWindowClick
     @Override
     public void onInfoWindowClick(Marker marker) {
         int position = venueNames.indexOf(marker.getTitle());
-        Intent i = new Intent(this, MCheckInActivity.class);
-        i.putExtra("selectedVenue", position);
-        startActivity(i);
+        if (venueCoordinates.get(position)[2].equals("checkable")) {
+            Intent i = new Intent(this, MCheckInActivity.class);
+            i.putExtra("selectedVenue", position);
+            startActivity(i);
+        }
     }
 
     @Override
@@ -73,7 +80,7 @@ public class MapActivity extends Activity implements GoogleMap.OnInfoWindowClick
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == android.R.id.home){
+        if (id == android.R.id.home) {
             this.finish();
             return true;
         }
